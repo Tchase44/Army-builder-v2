068c72c45701a61688dd15c97cf9694a65923d02
Tchase44 <tchase4544@gmail.com>
2017-08-08 19:31:39 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <units></units>
+  </div>
+</template>
+
+<script>
+import Units from "./Units.vue"
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    Units
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/Units.vue
@@ -0,0 +1,42 @@
+<template>
+	<main>
+		<h1>Lizardmen Vue</h1>
+		<div>
+			<div v-on:click="" >Heroes</div>
+			<div v-on:click="" >Leaders</div>
+			<div v-on:click="" >Units</div>
+		</div>
+		<div v-show="false" v-for="person in heroes">
+			<h3>{{person.name}}</h3>
+			<p>Health: {{person.wounds}}</p>
+			<p>Movement(inches): {{person.movement}}</p>
+			<p>Bravery: {{person.bravery}}</p>
+			<p>Save: {{person.save}}</p>
+			<h4>Weapons</h4>
+			<div v-for="weapons in person.weapons">
+				<h5>{{weapons.name}}</h5>
+				<p>Damage: {{weapons.damage}}</p>
+				<p>Range: {{weapons.range}}</p>
+			</div>
+		</div>	
+	</main>
+	
+</template>
+
+<script>
+	import army from "./assets/lizardmen"
+	export default {
+		name: "units",
+		data() {
+			return {
+				heroes: army.heroes,
+				leaders: army.leaders,
+				basic: army.basic,
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	
+</style>
