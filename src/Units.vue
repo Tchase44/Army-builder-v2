dfd2d3603cdb6e32e595dd139a7b75d003b92463
Tchase44 <tchase4544@gmail.com>
2017-08-22 18:24:19 -0400

    style

--- src/Units.vue
+++ src/Units.vue
@@ -1,23 +1,26 @@
 <template>
 	<main>
 		<h1>Lizardmen Vue</h1>
-		<div>
+		<div class="subhead">
 			<div v-on:click="changeShow(heroes)" >Heroes</div>
 			<div v-on:click="changeShow(leaders)" >Leaders</div>
 			<div v-on:click="changeShow(basic)" >Units</div>
 		</div>
-		<div v-for="person in current">
-			<h3>{{person.name}}</h3>
-			<p>Health: {{person.wounds}}</p>
-			<p>Movement(inches): {{person.movement}}</p>
-			<p>Bravery: {{person.bravery}}</p>
-			<p>Save: {{person.save}}</p>
-			<h4>Weapons</h4>
-			<div v-for="weapons in person.weapons">
-				<h5>{{weapons.name}}</h5>
-				<p>Damage: {{weapons.damage}}</p>
-				<p>Range: {{weapons.range}}</p>
+		<div class="cards" v-for="person in current">
+			<div class="card">
+				<h3>{{person.name}}</h3>
+				<p>Health: {{person.wounds}}</p>
+				<p>Movement(inches): {{person.movement}}</p>
+				<p>Bravery: {{person.bravery}}</p>
+				<p>Save: {{person.save}}</p>
+				<h4>Weapons</h4>
+				<div v-for="weapons in person.weapons">
+					<h5>{{weapons.name}}</h5>
+					<p>Damage: {{weapons.damage}}</p>
+					<p>Range: {{weapons.range}}</p>
+				</div>
 			</div>
+			<hr>
 		</div>	
 	</main>
 	
@@ -45,5 +48,20 @@
 </script>
 
 <style scoped>
-	
-</style>
+	main{
+		min-width:100vw;
+	}
+	h1{text-align: center;}
+	.subhead{
+		text-align: center;
+	}
+	.cards{
+		width: 100%;
+		text-align: center;
+	}
+	.card{
+		/*width: 30%;*/
+		/*display: inline-block;*/
+		/*border: solid black;*/
+	}
+</style>s
