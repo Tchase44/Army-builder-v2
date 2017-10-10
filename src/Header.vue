5d0adeb857f096a7157c1333f3acdb3ca8f403cc
Tchase44 <tchase4544@gmail.com>
2017-10-10 07:35:23 -0400

    carousel template start

---
+++ src/Header.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<transition-group class="carousel" tag="div">
+			<div 
+				v-for="slide in slides"
+				class="slide"
+				:key="slide.id">
+
+				<h4>{{slide.title}}</h4>
+			</div>
+		</transition-group>
+		<div class="carousel-controls">
+			<button class="carousel-controls_button" @click="next">Next</button>
+			<button class="carousel-controls_button" @click="last">Last</button>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "header",
+		data(){
+			return {
+
+			}
+		},
+		methods: {}
+	}
+</script>
+
+
+<style scoped></style>
