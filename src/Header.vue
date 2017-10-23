21f622e401302810785cf5a0fdba624fbd73d239
Tchase44 <tchase4544@gmail.com>
2017-10-23 17:17:20 -0400

    quick save

--- src/Header.vue
+++ src/Header.vue
@@ -6,7 +6,8 @@
 				class="slide"
 				:key="slide.id">
 
-				<h4>{{slide.title}}</h4>
+				<!-- <h4>{{slide.title}}</h4> -->
+				<img :src="slide.image">
 			</div>
 		</transition-group>
 		<div class="carousel-controls">
@@ -24,19 +25,24 @@
 				slides: [
 				{
 					title: "I'm a Slide One",
-					id: 11
+					id: 11,
+					image: "https://eternalhunt.files.wordpress.com/2013/08/lizardman-release-2.jpg"
 				},{
 					title: "I'm a Slide Two",
-					id: 22
+					id: 22,
+					image: "https://s-media-cache-ak0.pinimg.com/originals/05/75/75/057575b0a3cf8f2403a7835c559cd687.jpg"
 				},{
 					title: "I'm a Slide You See First",
-					id: 33
+					id: 33,
+					image: "https://i.redd.it/pe8oa65xc34z.png"
 				},{
 					title: "I'm a Slide Four",
-					id: 44
+					id: 44,
+					image: "https://i.pinimg.com/736x/b8/eb/64/b8eb64805b27e01fc35cdd6aeb5e34e3--lizardmen-warhammer-warhammer-fantasy.jpg"
 				},{
 					title: "I'm a Slide Five",
-					id: 55
+					id: 55,
+					image: "http://2.bp.blogspot.com/-UXhbP_BkN9Q/Vp0t0yQBXiI/AAAAAAAAJFY/16yeb8rLCxI/s1600/seraphon8.jpg"
 				}
 				],
 				left: "<",
@@ -51,6 +57,9 @@
 			last () {
 				const last = this.slides.pop()
 				this.slides = [last].concat(this.slides)
+			},
+			autoNext () {
+				setInterval(function(){this.next()},2000)
 			}
 		}
 	}
@@ -62,7 +71,7 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		height:100px;
+		height:300px;
 	}
 	.carousel{
 		display: flex;
@@ -87,5 +96,8 @@
 	}
 	.slide:last-of-type{
 		opacity: 0;
+	}
+	img{
+		width: 100%;
 	}
 </style>
