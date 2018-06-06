5a2c936b86b45033a93c782b5b4afbfda2008276
Tim Chase <tchase@taphere.com>
2018-06-06 15:00:23 -0400

    router added

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Units from './Units.vue'
+import Show from './Show.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {path: '/', component: Units},
+  {path: "/:name", component: Show, props: true}
+]
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
