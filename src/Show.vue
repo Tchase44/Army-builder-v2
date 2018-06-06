1c21d3c5daf58b36dac9b599336a92ab6fb77ac2
Tim Chase <tchase@taphere.com>
2018-06-06 14:59:23 -0400

    show page

---
+++ src/Show.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="show">
+    <h3>{{name}}</h3>
+    <div></div>
+  </div>
+</template>
+
+<script>
+	import army from "./assets/lizardmen"
+export default {
+  name: "Show",
+  props:['name'],
+  data (){
+    return {
+    }
+  },
+  created:{
+    // findUnit() {
+    //   army.forEach( (arrayOfUnits)=> {
+    //     arrayOfUnits.filter( (unitObj) => {
+    //       return unitObj.name === this.title
+    //     })
+    //   })
+    // }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
