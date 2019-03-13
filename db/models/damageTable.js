50fef62381502c432f8e5f95d9ccf9a51a7b6232
tchase44 <tchase4544@gmail.com>
2019-03-13 08:37:38 -0400

    db

--- db/models/damageTable.js
+++ db/models/damageTable.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose')
 
 const DamageTable = new mongoose.Schema({
-    rows:[
-      {
-        wounds_suffered: String,
-        move: String,
-        attack1_name: String,
-        attack1_value: String,
-        attack2_name: String,
-        attack2_value: String
-      }
-    ],
-    unit_id: {type: mongoose.Schema.Types.ObjectId},
-    unit_name: {type: String},
+  rows:[
+    {
+      wounds_suffered: String,
+      move: String,
+      attack1_name: String,
+      attack1_value: String,
+      attack2_name: String,
+      attack2_value: String
+    }
+  ],
+  unit_id: {type: mongoose.Schema.Types.ObjectId},
+  unit_name: {type: String},
 })
 
-mongoose.model("DamageTable",DamageTable)
+mongoose.model("DamageTable", DamageTable)
 module.exports = mongoose
